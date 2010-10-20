e5aa26663032924e5cb14e93c66dc2c8032e0620
Sebastian Neuner <sebastian.neuner@freenet.de>
2010-10-20 14:51:26 +0200

    merged tinymce extensions into html.php

--- inc/plugins/html.php
+++ inc/plugins/html.php
@@ -1,13 +1,55 @@
 <?php
 
-foreach(glob("inc/plugins/html/*.php") as $filename) include($filename);
-
 // "plugin" for plain html-formatted text
+// uses tinymce (if installed)
 // returns main as it is as page-body
 // returns empty header
 
 class HtmlPlugin {
+	
+	var $tinymcePath = 'jscripts/tiny_mce/';
+	var $browserPath = 'jscripts/tiny_mce/plugins/browser/browser.php';
+	
+	// <browser>
+	
+	function browserAviable() {
+		if (file_exists($this->browserPath)) return true;
+		else return false;
+	}
 
+	function getTMCEinitaddition() {
+		if($this->browserAviable() == true) {
+			$ret .= "	file_browser_callback : \"myCustomFileBrowser\",\n";
+			return $ret;
+		}
+		else return;
+	}
+	// </browser>
+	
+	function getJSaddition() {
+		if($this->browserAviable() == true) {
+			$ret .= "function myCustomFileBrowser(field_name, url, type, win) {\n";
+			$ret .= "   FileWindow=win;\n";
+			$ret .= "   window.open('../browser/browser.php?type=' + type,'Assets','width=600,height=600');\n";
+			$ret .= "}\n";
+			$ret .= "function CallBackReturn(field_name, url){\n";
+			$ret .= "  FileWindow.document.forms[0].elements[field_name].value =url ;\n";
+			$ret .= "}\n";
+			return $ret;
+		}
+		else return;
+	}
+	
+	// </browser>
+	// <tinymce>
+	
+	function tinymceAviable() {
+		if (file_exists($this->tinymcePath.'tiny_mce.js')) return true;
+		else return false;
+	}
+	
+	// </tinymce>
+	
 	function getBody($main) {
 		return $main;
 	}
@@ -27,8 +69,20 @@ class HtmlPlugin {
 	}
 	
 	function getEditorHead($main) {
-		global $PLUGINS;
-		$ret = $PLUGINS['tinymce']->getEditorHead();
+		$ret = "";
+		if($this->tinymceAviable() == true) {
+			$ret =  "<script type=\"text/javascript\" src=\"".$this->tinymcePath."tiny_mce.js\"></script>\n";
+			$ret .= "<script type=\"text/javascript\">\n";
+			$ret .= "tinyMCE.init({\n";
+			$ret .= "	theme : \"advanced\",\n";
+			$ret .= "	mode : \"textareas\",\n";
+			$ret .= "   plugins : \"fullpage\,template,advimage,\",\n";
+			$ret .= $this->getTMCEinitaddition();
+			$ret .= "   theme_advanced_buttons3_add : \"fullpage\"\n";
+			$ret .= "});\n";
+			$ret .= $this->getJSaddition();
+			$ret .= "</script>\n";
+		}
 		return $ret;
 	}
 }
